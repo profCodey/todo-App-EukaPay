aabf002e2df6a485b7de298cd522a591b9bdb0c3
Demilade Otunaiya <otunaiyademilade@gmail.com>
2024-12-10 19:54:31 +0100

    Delete backend/dist directory

--- backend/dist/routes/todoRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { TodoController } from '../controller/TodoController';
-const router = express.Router();
-router.get('/', TodoController.getAllTodos);
-router.post('/', TodoController.createTodo);
-router.put('/:id', TodoController.updateTodo);
-router.delete('/:id', TodoController.deleteTodo);
-export default router;
