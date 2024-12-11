61f3923e7c38bd6be3f40f578a081e9b32a3620d
profCodey <otunaiyademilade@gmail.com>
2024-12-11 09:25:30 +0100

    created front end repo files

---
+++ frontend/my-app/src/app/pages/ListItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListItem = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis provident ullam quos perferendis, explicabo laudantium delectus quaerat inventore nam voluptate beatae recusandae! Libero suscipit reprehenderit necessitatibus ab quo dolorem.
+    </div>
+  )
+}
+
+export default ListItem;
