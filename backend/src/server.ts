752943d0d4121ff13da21ebfe66b4f027d252332
profCodey <otunaiyademilade@gmail.com>
2024-12-11 19:08:32 +0100

    formatted code with prettier

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import todoRoutes from './routes/todoRoutes';
+import cors from 'cors';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.NEXT_PUBLIC_BASE_URL || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
 
